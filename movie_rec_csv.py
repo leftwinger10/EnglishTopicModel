ff7b91041383c2f25c75879427f3d95fb2bb68c6
leftwinger10 <chaohu1005@gmail.com>
2020-06-11 08:38:45 +0530

    init commit

--- movie_rec_csv.py
+++ movie_rec_csv.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle
+import time
 
 from gensim.matutils import jensen_shannon
 
@@ -22,6 +23,7 @@ df_title = pd.DataFrame({"title": [],
 for i in range(len(titles)):
     df_title.empty
     df_curr = pd.DataFrame({"title": [titles[i]]})
+    start = time.time()
     for j in range(len(lda_bow)):
         if j != i:
             dst = jensen_shannon(lda_bow[i], lda_bow[j], 130)
@@ -31,9 +33,17 @@ for i in range(len(titles)):
 
     df_title = df_title.sort_values(by=['dist'])
     df_rec = pd.DataFrame({"rec": [df_title['title'][0:50]]})
+    rec = df_rec["rec"][0].tolist()
+    df_rec = pd.DataFrame({"rec": [rec]})
     df_ind_res = pd.merge(df_curr, df_rec, left_index=True, right_index=True, how='inner')
     df_res = df_res.append(df_ind_res, ignore_index=True)
-    if i == 100:  # remove this or change this as requirement | Here i is number of movies in CSV
+    if i == 0:
+        df_ind_res.to_csv('movie_recommendation.csv', index=False)
+    else:
+        df_ind_res.to_csv('movie_recommendation.csv', mode='a', index=False, header=False)
+    if i == 99:  # remove this or change this as requirement | Here i is number of movies in CSV
         break
+    end = time.time()
+    print(i, end - start)
 
-df_res.to_csv('movie_recommendation_100.csv', index=False)
+
